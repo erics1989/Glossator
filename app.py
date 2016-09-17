2cef6dd27def420834da0a1548019915c9d2149b
Eric S <ericysim@gmail.com>
2016-09-17 16:04:41 -0400

    colored parts of speech, tested definitions

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, render_template
 from flask import request
+import requests
+import parser2
+import json
 
 app = Flask(__name__)
 
@@ -10,9 +13,14 @@ def glossator():
 @app.route('/', methods=['POST'])
 def glossator_parse():
     text = request.form['text']
-    processed_text = text.upper()
+    data = parser2.parse(text)
+    print(data)
 
-    return render_template('parsed.html', parsed="<b>hello</b>")
+    def_json_str = requests.get('http://api.pearson.com/v2/dictionaries/lasde/entries?headword=fence').json()
+    define = def_json_str["results"][0]["senses"][0]["definition"][0]
+
+
+    return render_template('parsed.html', words=data["words"], definition=define)
  
 
 
--- parser2.py
+++ parser2.py
@@ -18,5 +18,5 @@ def parse(text):
 
     return data
 
-print(parse("The cat jumped over the fence."))
+print("finished loading parser2")
 
