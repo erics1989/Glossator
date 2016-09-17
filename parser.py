e552f3bb9bbb0fb671ede6ffe61c64b1e84ce95f
Eric S <ericysim@gmail.com>
2016-09-17 14:31:30 -0400

    deleted __pycache__

---
+++ parser.py
@@ -0,0 +1,19 @@
+
+import spacy
+
+en = spacy.load("en")
+
+def parse(text):
+    doc = en(text)
+
+    data = {}
+    data["words"] = []
+
+    for token in doc:
+        word = {}
+        word["pos"] = token.pos_
+        word["lemma"] = token.lemma_
+        data["words"].append(word)
+
+    return data
+
